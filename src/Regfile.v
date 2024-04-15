99c764adcf1bd0d1b7a7473e2729d864ae5c26dd
RockRockWhite <1834309151@qq.com>
2024-04-15 15:36:07 +0800

    [Regfile] finished module regfile

--- src/Regfile.v
+++ src/Regfile.v
@@ -1,3 +1,5 @@
+`include "./Register_CE.v"
+
 module Regfile #(parameter N = 32)(
         input clk,
         input [4:0] rs1,
@@ -9,5 +11,25 @@ module Regfile #(parameter N = 32)(
         output [N-1:0] read_data2
     );
 
+    reg register_enable[31:0];
+    reg [N-1:0] register_read_data [31:0];
+
+    // generate the register file
+    generate
+        for(genvar i = 0; i !=32; i = i + 1) begin : regfile_block
+            Register_CE#(.N(N)) regfile(.q(register_read_data[i]), .d(write_data), .ce(register_enable[i]), .clk(clk));
+        end
+    endgenerate
+
+    // set register enable
+    always @(*) begin
+        for(integer i = 0; i != 32; i = i + 1) begin
+            register_enable[i] = (i[4:0] == rd) ? write_enable : 1'b0;
+        end
+    end
+
+    // set read data
+    assign read_data1 = register_read_data[rs1];
+    assign read_data2 = register_read_data[rs2];
 
 endmodule
