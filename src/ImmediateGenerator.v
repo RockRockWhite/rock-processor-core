06453831938baf9b5f1286151409356c3b2adee3
RockRockWhite <1834309151@qq.com>
2024-05-15 16:03:17 +0800

    [rpc] fix assign in always block

--- src/ImmediateGenerator.v
+++ src/ImmediateGenerator.v
@@ -1,8 +1,8 @@
-module ImmediateGenerator(
-        input [31:0] instruction,
-        input [2:0] immediate_select,
-        output reg [31:0] immediate
-    );
+module ImmediateGenerator (
+    input [31:0] instruction,
+    input [2:0] immediate_select,
+    output reg [31:0] immediate
+);
 
     // 001 I-type format
     // 010 S-type format
@@ -11,24 +11,43 @@ module ImmediateGenerator(
     // 101 J-type format
     // 110 CSR instructions
     always @(*) begin
-        case(immediate_select)
+        case (immediate_select)
             3'b001:
-                // I-type format
-                assign immediate = {{{20{instruction[31]}}, instruction[31:20]}};
+            // I-type format
+            immediate = {
+                {{20{instruction[31]}}, instruction[31:20]}
+            };
             3'b010:
-                // S-type format
-                assign immediate = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
+            // S-type format
+            immediate = {
+                {20{instruction[31]}}, instruction[31:25], instruction[11:7]
+            };
             3'b011:
-                // B-type format
-                assign immediate = {{19{instruction[31]}},  instruction[31], instruction[7],  instruction[30:25],  instruction[11:8], 1'b0};
+            // B-type format
+            immediate = {
+                {19{instruction[31]}},
+                instruction[31],
+                instruction[7],
+                instruction[30:25],
+                instruction[11:8],
+                1'b0
+            };
             3'b100:
-                // U-type format
-                assign immediate = {instruction[31:12], 12'b0};
+            // U-type format
+            immediate = {
+                instruction[31:12], 12'b0
+            };
             3'b101:
-                // J-type format
-                assign immediate = {{11{instruction[31]}}, instruction[31], instruction[19:12], instruction[20], instruction[30:21], 1'b0};
-            default:
-                assign immediate = 32'b0;
+            // J-type format
+            immediate = {
+                {11{instruction[31]}},
+                instruction[31],
+                instruction[19:12],
+                instruction[20],
+                instruction[30:21],
+                1'b0
+            };
+            default: immediate = 32'b0;
         endcase
     end
 endmodule
